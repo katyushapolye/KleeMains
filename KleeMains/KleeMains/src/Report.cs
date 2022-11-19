9e4fd9f547104e1e9a6671e32194c2d9f70f6209
Raphael Nogueira Rezende Laroca Pinto <61098580+katyushapolye@users.noreply.github.com>
2022-11-19 12:41:14 -0300

    Adjusts

--- KleeMains/KleeMains/src/Report.cs
+++ KleeMains/KleeMains/src/Report.cs
@@ -7,12 +7,19 @@ using System.Threading.Tasks;
 
 namespace KleeMains
 {
+
+    //a classe party gera o report -> la onde fica os calculos e sugestões
     class Report
     {
+       
         int maxPossibleDamage = 0;
+        int maxPossibleEM = 0;
+
         int totalEXPCost = 0;
         int totalMoraCost = 0;
         string SugestionBio = "";
+
+
         Elements elementalResonance;
 
            
